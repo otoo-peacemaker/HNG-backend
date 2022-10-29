18e555ca193c7cc54c11673bb433514d901d2c41
Peacemaker Otoo <infofeedacc@gmail.com>
2022-10-29 14:58:57 +0000

    changes

--- src/main/java/com/server/hng/UserController.java
+++ src/main/java/com/server/hng/UserController.java
@@ -1,25 +1,41 @@
 package com.server.hng;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("backend/gng/api/users")
 public class UserController {
 
     //DATABASE
-    private static final List<User> USERS = Arrays.asList(
+    private static final List<User> USERS = List.of(
             new User("Peacemaker", true, 28, "I am Software Engineer")
     );
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getUsers() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(Map.of(
+                      "slackName","Peacemaker"  ,
+                        "backend","true",
+                        "age","28",
+                        "bio","Software Engineer"
+                ));
+    }
+/*
     @GetMapping()
     public static List<User> getUser() {
         return USERS;
-    }
+    }*/
 
     @GetMapping(path = "{slackUserName}")
     public User getUserBySlackName(@PathVariable("slackUserName") String slackUserName) {
